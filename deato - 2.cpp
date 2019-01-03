22cc3d7432d6337506c198575dd69f8da261375d
Fishball pree <46342390+jd666666666666666@users.noreply.github.com>
2019-01-03 17:41:32 +0800

    Add files via upload

---
+++ deato - 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+ int main() {
+
+ char a;
+
+ cout<< "Please input a character: ";
+ cin>> a;
+ cout<< "The ASCII value of " << a << " is " << int(a) << endl;
+ 
+	return 0;
+}
+
