0977860feb6891e9b49674c2e26f100a877b18cf
Fishball pree <46342390+jd666666666666666@users.noreply.github.com>
2019-01-03 18:43:05 +0800

    Add files via upload

---
+++ deato - 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void linechar(char a)
+for(int i = 1 ; i <= 20; a++){
+	cout << "a";
+}
+cout << endl;
+
+void linechar(a b;num1)
+for(int d =1: d <= 20; d++){
+	cout << "a";
+}
+cout << endl;
+int main ()
+{
+	line char("*"); line char("@",); linechar ("*",10); linechar("#",15);
+	return 0;
+}
+
+
